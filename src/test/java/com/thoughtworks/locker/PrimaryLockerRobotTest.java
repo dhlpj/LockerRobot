5a4e29e34df0d5d51a0d3a5d86774ce12fab3e2b
Jie Peng <jie.peng@thoughtworks.com>
2020-09-17 16:58:39 +0800

    feat: bag in 1st locker when primary locker robot save bag and 1st with free capacity and 2nd is full

--- src/test/java/com/thoughtworks/locker/PrimaryLockerRobotTest.java
+++ src/test/java/com/thoughtworks/locker/PrimaryLockerRobotTest.java
@@ -21,4 +21,19 @@ public class PrimaryLockerRobotTest {
         assertEquals(Type.M, mediumTicket.getType());
         assertEquals(mediumBag, firstMediumLocker.take(mediumTicket));
     }
+
+    @Test
+    public void should_bag_in_1st_locker_and_return_medium_ticket_when_save_bag_given_robot_manage_1st_locker_with_free_capacity_2nd_is_full() {
+        Locker firstMediumLocker = new Locker(Type.M, 10);
+        Locker secondMediumLocker = new Locker(Type.M, 1);
+        secondMediumLocker.save(new Bag(Type.M));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(asList(firstMediumLocker, secondMediumLocker));
+        Bag mediumBag = new Bag(Type.M);
+
+        Ticket mediumTicket = primaryLockerRobot.save(mediumBag);
+
+        assertNotNull(mediumTicket);
+        assertEquals(Type.M, mediumTicket.getType());
+        assertEquals(mediumBag, firstMediumLocker.take(mediumTicket));
+    }
 }

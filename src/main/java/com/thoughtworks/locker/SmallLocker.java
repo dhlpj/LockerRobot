edef9d61264b53a099bddc143c4803893d6e1e34
Jie Peng <jie.peng@thoughtworks.com>
2020-09-17 14:51:06 +0800

    feat: should throw exception when small locker is full and save small bag

--- src/main/java/com/thoughtworks/locker/SmallLocker.java
+++ src/main/java/com/thoughtworks/locker/SmallLocker.java
@@ -1,12 +1,26 @@
 package com.thoughtworks.locker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SmallLocker {
     private final int capacity;
+    private Map<SmallTicket, SmallBag> ticketBagMap = new HashMap<>();
     public SmallLocker(int capacity) {
         this.capacity = capacity;
     }
 
     public SmallTicket save(SmallBag smallBag) {
-        return new SmallTicket();
+        if (isFull()) {
+            throw new FullCapacityException();
+        }
+
+        SmallTicket smallTicket = new SmallTicket();
+        ticketBagMap.put(smallTicket, smallBag);
+        return smallTicket;
+    }
+
+    private boolean isFull() {
+        return ticketBagMap.size() >= capacity;
     }
 }
--- src/test/java/com/thoughtworks/locker/SmallLockerTest.java
+++ src/test/java/com/thoughtworks/locker/SmallLockerTest.java
@@ -11,6 +11,16 @@ public class SmallLockerTest {
         SmallBag smallBag = new SmallBag();
 
         SmallTicket smallTicket = smallLocker.save(smallBag);
+
         assertNotNull(smallTicket);
+    }
+
+    @Test(expected = FullCapacityException.class)
+    public void should_throw_full_capacity_exception_when_save_bag_given_small_locker_is_full_and_small_bag() {
+        SmallLocker smallLocker = new SmallLocker(1);
+        smallLocker.save(new SmallBag());
+        SmallBag smallBag = new SmallBag();
+
+        smallLocker.save(smallBag);
     }
 }

0f3d119dac852697e55aff55cacb6d168a61f1ef
Jie Peng <jie.peng@thoughtworks.com>
2020-09-17 16:14:59 +0800

    feat: should throw full capacity exception when medium locker is full and save medium bag

--- src/main/java/com/thoughtworks/locker/MediumLocker.java
+++ src/main/java/com/thoughtworks/locker/MediumLocker.java
@@ -1,16 +1,26 @@
 package com.thoughtworks.locker;
 
 import com.thoughtworks.locker.bag.MediumBag;
+import com.thoughtworks.locker.exception.FullCapacityException;
 import com.thoughtworks.locker.ticket.MediumTicket;
+
+import java.util.HashMap;
 
 public class MediumLocker {
     private final int capacity;
+    private HashMap<MediumTicket, MediumBag> ticketBagMap = new HashMap<>();
 
     public MediumLocker(int capacity) {
         this.capacity = capacity;
     }
 
     public MediumTicket save(MediumBag mediumBag) {
-        return new MediumTicket();
+        if (ticketBagMap.size() >= capacity) {
+            throw new FullCapacityException();
+        }
+
+        MediumTicket mediumTicket = new MediumTicket();
+        ticketBagMap.put(mediumTicket, mediumBag);
+        return mediumTicket;
     }
 }
--- src/test/java/com/thoughtworks/locker/MediumLockerTest.java
+++ src/test/java/com/thoughtworks/locker/MediumLockerTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.locker;
 
 import com.thoughtworks.locker.bag.MediumBag;
+import com.thoughtworks.locker.exception.FullCapacityException;
 import com.thoughtworks.locker.ticket.MediumTicket;
 import org.junit.Test;
 
@@ -15,5 +16,14 @@ public class MediumLockerTest {
         MediumTicket mediumTicket = mediumLocker.save(mediumBag);
 
         assertNotNull(mediumTicket);
+    }
+
+    @Test(expected = FullCapacityException.class)
+    public void should_throw_full_capacity_exception_when_save_bag_given_medium_locker_is_full_and_medium_bag() {
+        MediumLocker mediumLocker = new MediumLocker(1);
+        mediumLocker.save(new MediumBag());
+        MediumBag mediumBag = new MediumBag();
+
+        mediumLocker.save(mediumBag);
     }
 }

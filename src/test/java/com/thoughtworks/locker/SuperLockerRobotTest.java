b74c1d5b675fee12da3bfa2d5c3f16269e9ae617
Jie Peng <jie.peng@thoughtworks.com>
2020-09-18 11:08:04 +0800

    feat: bag in 1st greater idle rate locker when super locker robot save bag and all lockers idle rate is equal

--- src/test/java/com/thoughtworks/locker/SuperLockerRobotTest.java
+++ src/test/java/com/thoughtworks/locker/SuperLockerRobotTest.java
@@ -35,6 +35,20 @@ public class SuperLockerRobotTest {
         assertEquals(largeBag, secondLargeLocker.take(largeTicket));
     }
 
+    @Test
+    public void should_bag_in_1st_locker_and_return_large_ticket_when_save_bag_given_robot_manage_1st_locker_with_10_total_5_free_capacity_and_2nd_with_8_total_4_free_capacity() {
+        Locker firstLargeLocker = initLocker(Type.L, 10, 5);
+        Locker secondLargeLocker = initLocker(Type.L, 8, 4);
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(asList(firstLargeLocker, secondLargeLocker));
+        Bag largeBag = new Bag(Type.L);
+
+        Ticket largeTicket = superLockerRobot.save(largeBag);
+
+        assertNotNull(largeTicket);
+        assertEquals(Type.L, largeTicket.getType());
+        assertEquals(largeBag, firstLargeLocker.take(largeTicket));
+    }
+
     private Locker initLocker(Type type, int capacity, int freeCapacity) {
         Locker locker = new Locker(type, capacity);
         for (int i = capacity; i > freeCapacity; i--) {

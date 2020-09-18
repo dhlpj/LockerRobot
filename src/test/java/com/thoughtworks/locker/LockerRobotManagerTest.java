5b52d46622d96c78139aa615fd0127101e2095d9
Jie Peng <jie.peng@thoughtworks.com>
2020-09-18 14:57:41 +0800

    feat: throw full capacity exception when super locker robot save small bag and managed small bag is full

--- src/test/java/com/thoughtworks/locker/LockerRobotManagerTest.java
+++ src/test/java/com/thoughtworks/locker/LockerRobotManagerTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.locker;
 
 import com.thoughtworks.locker.enums.Type;
+import com.thoughtworks.locker.exception.FullCapacityException;
 import com.thoughtworks.locker.robot.PrimaryLockerRobot;
 import com.thoughtworks.locker.robot.SuperLockerRobot;
 import org.junit.Test;
@@ -23,5 +24,17 @@ public class LockerRobotManagerTest {
         assertEquals(smallBag, smallLocker.take(smallTicket));
         assertNotNull(smallTicket);
         assertEquals(Type.S, smallTicket.getType());
+    }
+
+    @Test(expected = FullCapacityException.class)
+    public void should_throw_full_capacity_exception_when_save_small_bag_given_manager_manage_1_small_locker_is_full_and_robots_are_not_full() {
+        Locker smallLocker = new Locker(Type.S, 1);
+        smallLocker.save(new Bag(Type.S));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(singletonList(new Locker(Type.M, 10)));
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(singletonList(new Locker(Type.L, 10)));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(singletonList(smallLocker), singletonList(primaryLockerRobot), singletonList(superLockerRobot));
+        Bag smallBag = new Bag(Type.S);
+
+        lockerRobotManager.save(smallBag);
     }
 }
